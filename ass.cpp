d48055d21ffcac7fa4b4ff3a684fa0781aeec099
msnk3141 <76867193+msnk3141@users.noreply.github.com>
2023-06-20 03:07:37 +0800

    Partially refactored ass.cpp
    - Comments!
    - Used std global namespace
    - Removed unused GotoLine() function
    - Better variable names
    - Added function descriptions
    - Changed get_lines() to get_line_count()
    - Changed function signature of process_lines()
    - Merged the two while-loops in process_lines() to a single succinct for-loop
    - Kept but commented out scaffolding code for future testing
    - Removed broadcast of total line count in main(); variable not used by other processes
    - Combined root process tasks of calculating eachLines and handling the remaining lines

--- ass.cpp
+++ ass.cpp
@@ -1,3 +1,13 @@
+/*
+???
+
+Compile with:
+	$ mpic++ ass.cpp -o ass
+	
+Run with:
+	$ mpirun ./ass
+*/
+
 #include <iostream>
 #include <fstream>
 #include <string>
@@ -10,111 +20,139 @@
 
 using namespace std;
 
-std::fstream& GotoLine(std::fstream& file, unsigned int num){
-    file.seekg(std::ios::beg);
-    for(int i=0; i < num - 1; ++i){
-        file.ignore(std::numeric_limits<std::streamsize>::max(),'\n');
-    }
-    return file;
-}
+/*
+Comments!
+Used std global namespace
+Removed unused GotoLine() function
+Better variable names
+Added function descriptions
+Changed get_lines() to get_line_count(), 
+Changed function signature of process_lines()
+Merged the two while-loops in process_lines() to a single succinct for-loop
+Kept but commented out scaffolding for future testing 
+*/
 
-int get_lines(std::string filename) {
-	std::fstream inFile;
-	std::string line;
-	int count = 0;
+int get_line_count(string filename) {
+	/* Return the number of lines in a given text file. */
+	
+	fstream inFile; // input file object
+	string line; // temporary variable to store each line as a string of chars
+	int lineCount = 0;
 	
 	inFile.open(filename);
 	if (!inFile.is_open()) {
-		std::cout << "Error: could not open file" << std::endl;
+		cout << "Error: could not open file '" << filename << "'" << endl;
 		exit(1);
 	}
 	
-	while ( std::getline(inFile, line) ) {
+	// count the number of lines in the input file
+	while ( getline(inFile, line) ) {
 		if (line.back() == '\r')
-			line.erase(line.end() - 1); // remove last 'CR' or \r character
-		
-		//std::cout << line << " [" << line.size() << " bytes]" << std::endl;
-		// std::cout << line << std::endl;
-		count++;
+			line.erase(line.end() - 1); // remove last 'CR' or \r character if exists
+		lineCount++;
 	}
 	
-	std::cout << "# of lines read: " << count << std::endl;
+	// cout << "'" << filename << "' line count: " << lineCount << endl << endl;
 	
-	std::cout << std::endl;
 	inFile.close();
-	return count;
+	return lineCount;
 }
 
-void process_lines(std::string filename, Counter& counter, int start, int eachLine) {
-	fstream inFile;
-	string line;
-	stringstream ss;
-	string word;
-	int lineNumber = 0;
-	int wordCount = 0;
-	int end = start+eachLine;
-
+void process_lines(string filename, int eachLines, int startPos, Counter& counter) {
+	/* Count the words in a specified section of a given text file, using the given starting line 
+	position and number of lines to determine which section of the text file to process. Store the 
+	results in the provided Counter object.
+	*/
 	
+	// int rank; MPI_Comm_rank(MPI_COMM_WORLD, &rank);
+	
+	fstream inFile; // input file object
+	string line; // temporary variable to store each line as a string of chars
+	string word; // temporary variable to store each word
+	stringstream lineStream; // temporary stream buffer for each line
+	int endPos = startPos + eachLines; // ending line position
+	int processedLineCount = 0;
+
+	// cout << " rank:" << rank 
+		// << " startPos:" << startPos
+		// << " endPos:" << endPos 
+		// << endl;
+		
 	inFile.open(filename);
 	if (!inFile.is_open()) {
-		std::cout << "Error: could not open file" << std::endl;
+		cout << "Error: could not open file '" << filename << "'" << endl;
 		exit(1);
 	}
-
-	while ( lineNumber != start && std::getline(inFile, line) ) {		
-		lineNumber++;
-	}
-
-	while (lineNumber >= start && lineNumber <= start+eachLine && getline(inFile,line)){
-		if (end == lineNumber){
-			break;
-		}
+	
+	// Loop over each line in the text file. Start to process the text lines from the given 
+	// starting line position up to but not including the ending line position.
+	for (int pos = 0; pos < endPos; pos++) {
+		getline(inFile, line); // read the line at this position
+		if (pos < startPos)
+			continue; // advance to next iteration/line if starting line position is not yet reached
+		
 		if (line.back() == '\r')
-			line.erase(line.end() - 1); // remove last 'CR' or \r character
+			line.erase(line.end() - 1); // remove last 'CR' or \r character if exists
 		
-		// clear flags and set the stringstream buffer to the current line
-		ss.clear();
-		ss.str(line); 
+		// clear buffer flags and load the current line into stream buffer
+		lineStream.clear();
+		lineStream.str(line); 
 		
-		// process each word in this line
-		wordCount = 0;
-		while (ss >> word) {
+		// process each 'word' in this line, adding/updating the word counter
+		while (lineStream >> word) {
+			// word text cleaning goes here!
+			
 			update_counter(counter, word);
-			wordCount++;
 		}
-		lineNumber++;
+		processedLineCount++;
 	}
+	
+	// cout << " rank:" << rank 
+		// << " processedLineCount:" << processedLineCount
+		// << endl;
+	
 	inFile.close();
 }
 
 
 int main(int argc, char* argv[]) {
 	// MPI environment variables
-	int nprocs, rank, lineCount, eachLine; // number of processes and rank/id of each process
+	int nprocs, rank; // number of processes and rank/id of each process
 	
 	// Initialize the MPI environment
 	MPI_Init(&argc, &argv);
-
 
 	// Get the number of processes and rank of each process
 	MPI_Comm_size(MPI_COMM_WORLD, &nprocs);
 	MPI_Comm_rank(MPI_COMM_WORLD, &rank);
 	
-	char filename[] = "pg100.txt";
+	// Process variables
+	string filename = "uglyduck.txt";
+	int totalLines; // total number of lines in the file
+	int eachLines; // number of lines for each process to work on
+	int remLines; // number of remaining lies for root proccess to work on
+	Counter word_counter; // word counter of this process
 
-	Counter word_counter;
-
+	// Root proc determines the total number of lines and each process's line count
 	if (rank == ROOT) {
-		lineCount = get_lines(filename);
-		eachLine = lineCount/nprocs;
+		totalLines = get_line_count(filename);
+		eachLines = totalLines / nprocs; // divided equally between the processes
+		remLines = totalLines % nprocs;
+		
+		// cout << "totalLines:" << totalLines 
+			// << " eachLines:" << eachLines 
+			// << " remLines:" << remLines << endl;
+		
+		// handle work for remaining lines
+		process_lines(filename, remLines, (eachLines*nprocs), word_counter);
 	}
-	MPI_Bcast(&lineCount,1,MPI_INT,ROOT,MPI_COMM_WORLD);
-	MPI_Bcast(&eachLine,1,MPI_INT,ROOT,MPI_COMM_WORLD);
+	
+	// Broadcast each process's line count
+	MPI_Bcast(&eachLines, 1, MPI_INT, ROOT, MPI_COMM_WORLD);
 
-	process_lines(filename,word_counter,eachLine*rank, eachLine);
-	if (rank == ROOT){
-		process_lines(filename,word_counter,(eachLine*nprocs), lineCount%nprocs);
-	}
+	// Each process works on their section of the text file
+	process_lines(filename, eachLines, (eachLines*rank), word_counter);
+	
 	MPI_Barrier(MPI_COMM_WORLD);
 
 	/*----------------
@@ -211,7 +249,8 @@ int main(int argc, char* argv[]) {
 
 	if (!rank){
 		compose_counter(total_counter,keysTotal,valuesTotal);
-		print_counter(total_counter);
+		cout << "Contents of the total_counter:-" << endl;
+		// print_counter(total_counter);
 	}
 
     delete[] dictKeys;

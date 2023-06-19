ab605d30a802b7258fca5a5d25abc670ed67e0eb
msnk3141 <76867193+msnk3141@users.noreply.github.com>
2023-06-19 21:55:05 +0800

    Kevin's edits
    - Parallelized word counter in ass.cpp main file
    - Additional and overloaded functions in Counter.h file
    
    Co-Authored-By: Kevin-Chandra <78064710+Kevin-Chandra@users.noreply.github.com>

--- Counter.h
+++ Counter.h
@@ -3,6 +3,8 @@
 #include <sstream>
 #include <vector>
 
+using namespace std;
+
 typedef std::unordered_map<std::string, int> Counter;
 
 void print_counter_content(Counter counter, std::string name) {
@@ -20,7 +22,23 @@ void add_counter_item(Counter& counter, std::string key) {
 		counter[key] = 1; // ... set new item count to 1
 }
 
-void decompose_counter(Counter counter, std::string& keys, std::vector<int>& values) {
+vector<string> getKeys(Counter& counter){
+	vector<string> keys;
+	for (auto& item: counter){
+		keys.push_back(item.first);
+	}
+	return keys;
+}
+
+vector<int> getValues(Counter& counter){
+	vector<int> values;
+	for (auto& item: counter){
+		values.push_back(item.second);
+	}
+	return values;
+}
+
+void decompose_counter(Counter& counter, std::string& keys, std::vector<int>& values) {
 	std::stringstream ss;
 	for (auto& item: counter) {
 		ss << item.first << '\n';
@@ -36,4 +54,14 @@ void compose_counter(Counter& counter, std::string keys, std::vector<int> values
 	int i = 0;
 	while (ss >> key)
 		counter[key] = values[i++];
+}
+
+void compose_counter(Counter& counter, vector<string> keys, vector<int> values) {
+	for (int i=0 ; i<keys.size(); i++){
+		if (counter.find(keys[i]) == counter.end()){ //not found
+			counter[keys[i]] = values[i];
+		} else {
+			counter.at(keys[i]) += values[i];
+		}
+	}
 }
--- ass.cpp
+++ ass.cpp
@@ -2,13 +2,24 @@
 #include <fstream>
 #include <string>
 #include <sstream>
+#include <limits>
 #include <mpi.h>
 #include "Counter.h"
 
 #define ROOT 0
 
-void print_lines(std::string filename) {
-	std::ifstream inFile;
+using namespace std;
+
+std::fstream& GotoLine(std::fstream& file, unsigned int num){
+    file.seekg(std::ios::beg);
+    for(int i=0; i < num - 1; ++i){
+        file.ignore(std::numeric_limits<std::streamsize>::max(),'\n');
+    }
+    return file;
+}
+
+int get_lines(std::string filename) {
+	std::fstream inFile;
 	std::string line;
 	int count = 0;
 	
@@ -23,7 +34,7 @@ void print_lines(std::string filename) {
 			line.erase(line.end() - 1); // remove last 'CR' or \r character
 		
 		//std::cout << line << " [" << line.size() << " bytes]" << std::endl;
-		std::cout << line << std::endl;
+		// std::cout << line << std::endl;
 		count++;
 	}
 	
@@ -31,24 +42,33 @@ void print_lines(std::string filename) {
 	
 	std::cout << std::endl;
 	inFile.close();
+	return count;
 }
 
-void process_lines(std::string filename, Counter& counter) {
-	std::ifstream inFile;
-	std::string line;
-	std::stringstream ss;
-	std::string word;
-	int lineCount = 0;
+void process_lines(std::string filename, Counter& counter, int start, int eachLine) {
+	fstream inFile;
+	string line;
+	stringstream ss;
+	string word;
+	int lineNumber = 0;
 	int wordCount = 0;
+	int end = start+eachLine;
+
 	
 	inFile.open(filename);
 	if (!inFile.is_open()) {
 		std::cout << "Error: could not open file" << std::endl;
 		exit(1);
 	}
-	
-	// process by line
-	while ( std::getline(inFile, line) ) {
+
+	while ( lineNumber != start && std::getline(inFile, line) ) {		
+		lineNumber++;
+	}
+
+	while (lineNumber >= start && lineNumber <= start+eachLine && getline(inFile,line)){
+		if (end == lineNumber){
+			break;
+		}
 		if (line.back() == '\r')
 			line.erase(line.end() - 1); // remove last 'CR' or \r character
 		
@@ -62,36 +82,146 @@ void process_lines(std::string filename, Counter& counter) {
 			add_counter_item(counter, word);
 			wordCount++;
 		}
-		
-		lineCount++;
-		//std::cout << "Line #" << lineCount << ": " << wordCount << " words processed" << std::endl;
+		lineNumber++;
 	}
-	
-	std::cout << std::endl;
 	inFile.close();
 }
 
+
 int main(int argc, char* argv[]) {
 	// MPI environment variables
-	int nprocs, rank; // number of processes and rank/id of each process
+	int nprocs, rank, lineCount, eachLine; // number of processes and rank/id of each process
 	
 	// Initialize the MPI environment
 	MPI_Init(&argc, &argv);
+
 
 	// Get the number of processes and rank of each process
 	MPI_Comm_size(MPI_COMM_WORLD, &nprocs);
 	MPI_Comm_rank(MPI_COMM_WORLD, &rank);
 	
 	char filename[] = "pg100.txt";
+
 	Counter word_counter;
-	
+
 	if (rank == ROOT) {
-		//print_lines(filename);
-		process_lines(filename, word_counter);
-		print_counter_content(word_counter, "word counter");
-		std::cout << "Unique words: " << word_counter.size() << std::endl;
+		lineCount = get_lines(filename);
+		eachLine = lineCount/nprocs;
 	}
+	MPI_Bcast(&lineCount,1,MPI_INT,ROOT,MPI_COMM_WORLD);
+	MPI_Bcast(&eachLine,1,MPI_INT,ROOT,MPI_COMM_WORLD);
+
+	process_lines(filename,word_counter,eachLine*rank, eachLine);
+	if (rank == ROOT){
+		process_lines(filename,word_counter,(eachLine*nprocs), lineCount%nprocs);
+	}
+	MPI_Barrier(MPI_COMM_WORLD);
+
+	/*----------------
+		gather values
+	------------------*/
+
+	auto values = getValues(word_counter);
+	auto keys = getKeys(word_counter);
+	int dataSize = values.size();
+
+	int recvIntCount[nprocs];
+
+	MPI_Gather(&dataSize, 1, MPI_INT,recvIntCount, 1, MPI_INT,ROOT, MPI_COMM_WORLD);
+
+	int totlen = 0;
+    int displsValues[nprocs];
+    int *totalInt = NULL;
+
+    if (rank == ROOT) {
+
+        displsValues[0] = 0;
+        totlen += recvIntCount[0];
+
+        for (int i=1; i<nprocs; i++) {
+           totlen += recvIntCount[i];
+           displsValues[i] = displsValues[i-1] + recvIntCount[i-1];
+        }
+        totalInt = (int*) malloc(totlen * sizeof(int));
+    }
+
+	MPI_Barrier(MPI_COMM_WORLD);
+
+	MPI_Gatherv(&values[0], dataSize, MPI_INT, totalInt, recvIntCount, displsValues, MPI_INT,ROOT, MPI_COMM_WORLD);
 	
+	/*----------------
+		gather keys
+	------------------*/
+	int maxKeyLength = 150;
+
+	int numKeys = keys.size();
+    int totalLength = numKeys * maxKeyLength;
+    int finalNumKeys = 0;
+    MPI_Reduce(&numKeys, &finalNumKeys, 1, MPI_INT, MPI_SUM, 0, MPI_COMM_WORLD);
+
+    // Computing number of elements that are received from each process
+    int *recvcounts = NULL;
+    if (rank == 0)
+        recvcounts = new int[nprocs];
+
+    MPI_Gather(&totalLength, 1, MPI_INT, recvcounts, 1, MPI_INT, 0, MPI_COMM_WORLD);
+
+    // Computing displacement relative to recvbuf at which to place the incoming data from each process
+    int *displs = NULL;
+    if (rank == 0)
+    {
+        displs = new int[nprocs];
+
+        displs[0] = 0;
+        for (int i = 1; i < nprocs; i++)
+            displs[i] = displs[i - 1] + recvcounts[i - 1];
+    }
+
+    char(*dictKeys)[maxKeyLength];
+    char(*finalDictKeys)[maxKeyLength];
+    dictKeys = (char(*)[maxKeyLength])malloc(numKeys * sizeof(*dictKeys));
+    if (rank == 0)
+        finalDictKeys = (char(*)[maxKeyLength])malloc(finalNumKeys * sizeof(*finalDictKeys));
+
+    // Collect keys for each process
+    int i = 0;
+    for (auto str : keys)
+    {
+        strncpy(dictKeys[i], str.c_str(), maxKeyLength);
+        i++;
+    }
+
+    MPI_Gatherv(dictKeys, totalLength, MPI_CHAR, finalDictKeys, recvcounts, displs, MPI_CHAR, 0, MPI_COMM_WORLD);
+
+    // Create new counter and merge all previous counter
+
+	vector<string> keysTotal;
+	vector<int> valuesTotal;
+
+	if (!rank){
+		for (int i = 0; i<totlen;i++){
+			keysTotal.push_back((string)finalDictKeys[i]);
+			valuesTotal.push_back(totalInt[i]);
+			// cout << finalDictKeys[i]  << " : " << totalInt[i] << endl;
+		}
+	}
+
+
+	Counter total_counter;
+
+	if (!rank){
+		compose_counter(total_counter,keysTotal,valuesTotal);
+		print_counter_content(total_counter,"All");
+	}
+
+    delete[] dictKeys;
+    if (rank == 0)
+    {
+        delete[] finalDictKeys;
+        delete[] recvcounts;
+        delete[] displs;
+    }
+
 	// Finalize the MPI environment.
     MPI_Finalize();
 	
